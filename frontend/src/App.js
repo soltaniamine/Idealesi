2352e6ec2042085ad407bda0bd956054e893db81
Mahdi-Boudraa <160179107+Mahdi-Boudraa@users.noreply.github.com>
2024-03-19 02:03:01 +0100

    first integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,31 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-<<<<<<< Updated upstream
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
     
     return ( 
         <div></div>
      );
->>>>>>> Stashed changes
 }
  
 export default App;
