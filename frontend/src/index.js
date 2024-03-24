b72b6fe92a1f51630d5a963277aefac0c19793cd
soltaniamine <lm_soltani@esi.dz>
2024-03-24 20:30:04 +0100

    Shared with me page added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,8 @@ import './index.css';
 // import Sidebar from './Pages/Accueil/Home/Sidebar';
 // import Home from './Pages/Accueil/Home/Home';
 // import Homepage from './Pages/Accueil/Home/Homepage';
-import Favorite from './Pages/Accueil/Favorite/Favorite';
+// import Favorite from './Pages/Accueil/Favorite/Favorite';
+import Shared from './Pages/Accueil/Sharedwithme/Sharedwithme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -20,7 +21,8 @@ root.render(
     {/* <Sidebar/> */}
     {/* <Home/> */}
     {/* <Homepage/> */}
-    <Favorite/>
+    {/* <Favorite/> */}
+    <Shared/>
   </React.StrictMode>
 );
 
